459ad37676fd90f387de9d6ae364b4e9b47613ab
Dlhuutien <Danglehuutien000@gmail.com>
2023-10-01 16:53:35 +0700

    Update

--- JS/inLove.js
+++ JS/inLove.js
@@ -160,3 +160,12 @@ $(document).ready(function($) {
   
   
   
+// Lắng nghe sự kiện cuộn trang
+window.addEventListener("scroll", function() {
+    const nav = document.querySelector("nav");
+    if (window.scrollY > 0) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+});
